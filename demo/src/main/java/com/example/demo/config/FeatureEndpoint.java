0ed05841a397f35c54a40d08b0d295faa87ebc02
shakib522 <shakibhasann522@gmail.com>
2023-07-23 18:43:37 +0600

    Exclude some Spring actuator endpoint

--- demo/src/main/java/com/example/demo/config/FeatureEndpoint.java
+++ demo/src/main/java/com/example/demo/config/FeatureEndpoint.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 @Component
 @Endpoint(id="features")
